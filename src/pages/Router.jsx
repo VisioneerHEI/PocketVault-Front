fab9df722909fa3760e638d2dda26e9c460fa2ad
tendryAxel <hei.tendry@gmail.com>
2024-02-03 09:18:02 +0300

    feat: error page

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,15 +1,20 @@
 import { Flex } from "@chakra-ui/react";
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ErrorPage from "./Error";
 import Login from "./login";
 import SignUp from "./signup";
 
 const Router = () => {
+    const [error, setError] = useState(200);
+
     return (
         <Flex>
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<SignUp />} />
+                    <Route path="*" element={<ErrorPage error_code={error} />} />
                 </Routes>
             </BrowserRouter>
         </Flex>
