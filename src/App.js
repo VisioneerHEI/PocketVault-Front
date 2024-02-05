907775c4c3c847e7aaab77978226e9a1ad224c0b
tendryAxel <hei.tendry@gmail.com>
2024-02-05 13:52:00 +0300

    fix: eslint code check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Router from './pages/Router';
-import { Flex } from '@chakra-ui/react';
+import React, { Flex } from '@chakra-ui/react';
 
 function App() {
   return (
