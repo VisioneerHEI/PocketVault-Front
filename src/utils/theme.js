9055222569ba1032dc00223fa140bb89c71e6a42
tendryAxel <hei.tendry@gmail.com>
2024-02-07 18:39:58 +0300

    feat: create theme color

---
+++ src/utils/theme.js
@@ -0,0 +1,14 @@
+const theme = ["f4f1bb","3d405b","ff8966","918ef4","69a297"];
+
+
+const formatColorHexa = (c) => {
+    return `#${c}`
+};
+
+export const color = {
+    "light": formatColorHexa(theme[0]),
+    "dark": formatColorHexa(theme[1]),
+    "red": formatColorHexa(theme[2]),
+    "primary": formatColorHexa(theme[4]),
+    "second": formatColorHexa(theme[3])
+};
