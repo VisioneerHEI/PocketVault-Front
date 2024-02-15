dc8007659ee0ab9330fe81a5ad316403954de722
tendryAxel <hei.tendry@gmail.com>
2024-02-15 15:05:15 +0300

    feat: solde user local storage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import ErrorPage from '../pages/Error'
 import { createContext } from 'react'
 import * as token from '../utils/token.js'
+import * as st from '../utils/userStorage'
 
 const LoadError = () => {
   useEffect(() => {
@@ -19,8 +20,20 @@ const LoadError = () => {
 const UserContext = createContext()
 
 const AuthProvider = ({ children }) => {
+  const getSolde = () => {
+    return st.get("solde") || 0
+  };
+  const addMoney = (v) => {
+    st.set("solde", getSolde() + v)
+    return st.get("solde")
+  };
+  const getBackMoney = (v) => {
+    st.set("solde", getSolde() - v)
+    return st.get("solde")
+  };
+
   return (
-    <UserContext.Provider value={{}}>
+    <UserContext.Provider value={{ getSolde, addMoney, getBackMoney }}>
       {token.get() ?
         children :
         <LoadError />
